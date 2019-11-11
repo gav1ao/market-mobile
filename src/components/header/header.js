87da2400b47f28143c3c7b1147b1c3fbd66e82a0
Vítor Gavião <vigal.jan@gmail.com>
2019-11-10 23:23:25 -0300

    Criação do componente de Header padronizado para o APP

---
+++ src/components/header/header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import {
+    Body,
+    Button,
+    Icon,
+    Header,
+    Left,
+    Right,
+    Subtitle,
+    Title
+} from 'native-base';
+
+export default function HeaderAPP ( props ) {
+
+    const onClickButton = () => {
+        if (props.header) {
+            props.header.backFunction();
+        }
+    }
+
+    return (
+        <Header>
+            <Left>
+                <Button
+                    transparent
+                    onPress={() => onClickButton()}
+                >
+                    <Icon name='arrow-back' />
+                </Button>
+            </Left>
+            <Body style={styles.body}>
+                <Title>{props.header.title}</Title>
+                <Subtitle>{props.header.subtitle}</Subtitle>
+            </Body>
+        </Header>
+    );
+}
+
+const styles = StyleSheet.create({
+    body: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: -100,
+    }
+})
