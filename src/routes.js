fa12568d4997b492790f62ce8b675abd4b440094
Vítor Gavião <vigal.jan@gmail.com>
2022-09-11 11:55:03 -0300

    Atualizando Expo - Parte 2

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
 
 import DummyPage from './pages/dummy-page/dummy-page';
 import Home from './pages/home/home';
@@ -7,11 +8,18 @@ import AddInvoiceCode from './pages/add-invoice-code/add-invoice-code';
 import SearchProduct from './pages/search-product/search-product';
 
 export default createAppContainer (
+    /*
     createSwitchNavigator({
         Home,
         DummyPage,
         SearchProduct,
         QRCodeReader,
         AddInvoiceCode,
+    })
+    */
+    createStackNavigator({
+        Home: {
+            screen: DummyPage,
+        },
     })
 );
